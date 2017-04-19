eb15f03d7d60a98ef3d8abc98cc90920eaa21fe2
金飞 <28206254@qq.com>
2017-04-19 13:45:37 +0800

    s

---
+++ Quartz.NetDemo/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quartz.NetDemo
+{
+    class Class1
+    {
+        public Class1() {
+            var i = 0;
+        }
+    }
+}
